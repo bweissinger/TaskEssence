47424ffd40586819491995225b0562311f7112d1
bweissinger <weissingerb@gmail.com>
2019-10-03 03:52:35 -0500

    Correct typo taskItems.set(i, taskItems.set(...))

--- app/src/main/java/com/madabysslabs/app/taskessence/CurrentTasksView.java
+++ app/src/main/java/com/madabysslabs/app/taskessence/CurrentTasksView.java
@@ -68,7 +68,7 @@ public class CurrentTasksView extends BaseFragment {
 
         for (int i = 0; i < taskItems.size(); i++){
             if(taskItems.get(i).isCompleted()){
-                taskItems.set(i, taskItems.set(i, new TaskItem("", taskItems.get(i).getColorId(), true, true)));
+                taskItems.set(i, new TaskItem("", taskItems.get(i).getColorId(), true, true));
             }
         }
 
